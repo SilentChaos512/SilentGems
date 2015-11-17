29a6d594db91ae9b467f3d3ea3c555d05dd84b0d
SilentChaos512 <sworkman256@gmail.com>
2015-11-17 13:22:05 -0600

    Fluffy blocks now reduces fall damage
    ...instead of preventing it. The amount depends on how many fluffy
    blocks are below the one landed on. If enough are present, fall damage
    is entirely negated.

--- common/net/silentchaos512/gems/block/BlockFluffyBlock.java
+++ common/net/silentchaos512/gems/block/BlockFluffyBlock.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.world.World;
+import net.silentchaos512.gems.core.util.LogHelper;
 import net.silentchaos512.gems.lib.Names;
 
 public class BlockFluffyBlock extends BlockSG {
@@ -20,8 +21,19 @@ public class BlockFluffyBlock extends BlockSG {
   }
 
   @Override
-  public void onFallenUpon(World world, int x, int y, int z, Entity entity, float f) {
+  public void onFallenUpon(World world, int x, int y, int z, Entity entity, float distance) {
 
-    entity.fallDistance = 0f;
+    int stackedBlocks = 0;
+    for (int depth = 0; y - depth > 0 && world.getBlock(x, y - depth, z) == this; ++depth) {
+      LogHelper.debug("y=" + (y - depth));
+      if (world.getBlock(x, y - depth, z) == this) {
+        ++stackedBlocks;
+      }
+    }
+
+    // Reduce fall distance by 10 blocks per stacked block
+    LogHelper.debug(entity.fallDistance + ", " +  distance + ", " + stackedBlocks);
+    entity.fallDistance -= Math.min(10 * stackedBlocks, distance);
+    LogHelper.debug(entity.fallDistance);
   }
 }
