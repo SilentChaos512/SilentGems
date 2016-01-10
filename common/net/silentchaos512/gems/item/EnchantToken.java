79d5afdfea1792d24f9c9dfec6466ab497c9f493
SilentChaos512 <sworkman256@gmail.com>
2016-01-09 21:09:08 -0600

    Token multi-line tool list

--- common/net/silentchaos512/gems/item/EnchantToken.java
+++ common/net/silentchaos512/gems/item/EnchantToken.java
@@ -240,7 +240,10 @@ public class EnchantToken extends ItemSG {
       }
       // List of valid tools.
       list.add(LocalizationHelper.getItemDescription(itemName, 2));
-      list.add(validToolsFor(meta));
+      String[] array = validToolsFor(meta).split("\n");
+      for (String str : array) {
+        list.add("  " + str);
+      }
       // Max level
       s = LocalizationHelper.getOtherItemKey(itemName, "MaxLevel");
       s += " " + enchants.get(meta).getMaxLevel();
@@ -584,11 +587,16 @@ public class EnchantToken extends ItemSG {
       }
 
       String s = "";
+      boolean split = false;
 
       // Separate each item with commas.
       for (Object o : list) {
         if (s.length() > 0) {
           s += ", ";
+          if (s.length() > 50 && !split) {
+            s += "\n";
+            split = true;
+          }
         }
         s += o;
       }
