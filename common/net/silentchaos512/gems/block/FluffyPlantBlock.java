9e4caab8d0f4bd39c0e3ada302a058e8b1ee98d2
SilentChaos512 <sworkman256@gmail.com>
2016-02-10 13:09:22 -0600

    Fluffy plant right-click to harvest (Closes #11)

--- common/net/silentchaos512/gems/block/FluffyPlantBlock.java
+++ common/net/silentchaos512/gems/block/FluffyPlantBlock.java
@@ -1,10 +1,21 @@
 package net.silentchaos512.gems.block;
 
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockCrops;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
 import net.silentchaos512.gems.SilentGems;
 import net.silentchaos512.gems.core.registry.IHasVariants;
+import net.silentchaos512.gems.core.util.LogHelper;
 import net.silentchaos512.gems.item.ModItems;
 import net.silentchaos512.gems.lib.Names;
 
@@ -15,6 +26,34 @@ public class FluffyPlantBlock extends BlockCrops implements IHasVariants {
   public FluffyPlantBlock() {
 
     setUnlocalizedName(Names.FLUFFY_PLANT);
+  }
+
+  @Override
+  public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player,
+      EnumFacing side, float hitX, float hitY, float hitZ) {
+
+    // Right-click to harvest
+    List<ItemStack> drops = Lists.newArrayList();
+    int age = state.getValue(AGE);
+
+    // Get drops if mature
+    if (age >= 7) {
+      for (int i = 0; i < 3; ++i) {
+        if (RANDOM.nextInt(15) <= age) {
+          drops.add(new ItemStack(getCrop(), 1, damageDropped(state)));
+        }
+      }
+
+      // Reset to newly planted state
+      world.setBlockState(pos, getDefaultState());
+    }
+
+    // Spawn items in world
+    for (ItemStack stack : drops) {
+      spawnAsEntity(world, pos, stack);
+    }
+
+    return !drops.isEmpty();
   }
 
   @Override
