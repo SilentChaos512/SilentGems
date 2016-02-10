2bb8c846a7b4b4a4854f04c1e66bd7b62783c17e
SilentChaos512 <sworkman256@gmail.com>
2016-02-10 13:25:31 -0600

    Fluffy plant right-click to harvest

--- common/net/silentchaos512/gems/block/FluffyPlantBlock.java
+++ common/net/silentchaos512/gems/block/FluffyPlantBlock.java
@@ -1,10 +1,19 @@
 package net.silentchaos512.gems.block;
 
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
 import net.minecraft.block.BlockCrops;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.SpawnerAnimals;
+import net.minecraft.world.World;
 import net.silentchaos512.gems.core.registry.SRegistry;
+import net.silentchaos512.gems.core.util.LogHelper;
 import net.silentchaos512.gems.lib.Names;
 import net.silentchaos512.gems.lib.Strings;
 
@@ -16,6 +25,34 @@ public class FluffyPlantBlock extends BlockCrops {
 
     this.setBlockName(Names.FLUFFY_PLANT);
     this.setBlockTextureName(Strings.RESOURCE_PREFIX + Names.FLUFFY_PLANT);
+  }
+
+  @Override
+  public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player,
+      int side, float hitX, float hitY, float hitZ) {
+
+    // Right-click to harvest
+    List<ItemStack> drops = Lists.newArrayList();
+    int age = world.getBlockMetadata(x, y, z);
+
+    // Get drops if mature
+    if (age >= 7) {
+      for (int i = 0; i < 3; ++i) {
+        if (i == 0 || world.rand.nextInt(15) <= age) {
+          drops.add(new ItemStack(func_149866_i(), 1, damageDropped(age)));
+        }
+      }
+
+      // Reset to newly planted state
+      world.setBlockMetadataWithNotify(x, y, z, 0, 2);
+    }
+
+    // Spawn items in world
+    for (ItemStack stack : drops) {
+      dropBlockAsItem(world, x, y, z, stack);
+    }
+
+    return !drops.isEmpty();
   }
 
   @Override
