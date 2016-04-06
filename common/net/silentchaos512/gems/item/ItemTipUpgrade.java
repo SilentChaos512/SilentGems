9b30a2598d141e4f38f054ba2888012add3be5d7
SilentChaos512 <sworkman256@gmail.com>
2016-04-05 20:38:32 -0500

    Add missing tipped upgrade tooltips

--- common/net/silentchaos512/gems/item/ItemTipUpgrade.java
+++ common/net/silentchaos512/gems/item/ItemTipUpgrade.java
@@ -13,10 +13,12 @@ import net.minecraftforge.oredict.ShapelessOreRecipe;
 import net.silentchaos512.gems.SilentGems;
 import net.silentchaos512.gems.api.tool.part.ToolPart;
 import net.silentchaos512.gems.api.tool.part.ToolPartRegistry;
+import net.silentchaos512.gems.api.tool.part.ToolPartTip;
 import net.silentchaos512.gems.lib.EnumTipUpgrade;
 import net.silentchaos512.gems.lib.Names;
 import net.silentchaos512.gems.util.ToolHelper;
 import net.silentchaos512.lib.item.ItemSL;
+import net.silentchaos512.lib.util.LocalizationHelper;
 
 public class ItemTipUpgrade extends ItemSL {
 
@@ -31,7 +33,34 @@ public class ItemTipUpgrade extends ItemSL {
   @Override
   public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean advanced) {
 
-    // TODO
+    ToolPartTip part = (ToolPartTip) ToolPartRegistry.fromStack(stack);
+    if (part != null) {
+      LocalizationHelper loc = SilentGems.instance.localizationHelper;
+      String line;
+
+      list.add(loc.getItemSubText(itemName, "willReplace"));
+
+      line = loc.getItemSubText(itemName, "harvestLevel");
+      list.add(String.format(line, part.getHarvestLevel()));
+
+      tooltipLine(loc, list, "durability", part.getDurability());
+      tooltipLine(loc, list, "harvestSpeed", part.getHarvestSpeed());
+      tooltipLine(loc, list, "attackSpeed", part.getMeleeSpeed());
+      tooltipLine(loc, list, "meleeDamage", part.getMeleeDamage());
+      tooltipLine(loc, list, "magicDamage", part.getMagicDamage());
+    }
+  }
+
+  protected void tooltipLine(LocalizationHelper loc, List list, String key, Number value) {
+
+    if (value.floatValue() == 0) {
+      return;
+    }
+
+    String line = loc.getItemSubText(itemName, key);
+    String numberString = (value.floatValue() < 0 ? "-" : "+")
+        + String.format(value instanceof Integer ? "%d" : "%.1f", value);
+    list.add(String.format(line, numberString));
   }
 
   // public EnumTipUpgrade getTipForUpgrade(int meta) {
