d005842c3de7462906e83c05a90d783f9eb1b8a2
SilentChaos512 <sworkman256@gmail.com>
2016-05-14 22:34:37 -0500

    Add IPartProperties

---
+++ common/net/silentchaos512/gems/api/tool/part/IPartProperties.java
@@ -0,0 +1,33 @@
+package net.silentchaos512.gems.api.tool.part;
+
+import net.minecraft.item.ItemStack;
+import net.silentchaos512.gems.api.lib.EnumMaterialTier;
+
+public interface IPartProperties {
+
+  public String getName();
+
+  public int getColor();
+
+  public EnumMaterialTier getTier();
+
+  public int getDurability();
+
+  public float getMiningSpeed();
+
+  public float getMeleeDamage();
+
+  public float getMagicDamage();
+
+  public float getMeleeSpeed();
+
+  public int getEnchantability();
+
+  public float getChargeSpeed();
+
+  public int getProtection();
+
+  public ItemStack getCraftingStack();
+
+  public String getCraftingOreName();
+}
