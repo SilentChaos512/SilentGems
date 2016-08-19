1b350bb1ae49dce113793e2315d1e86ffad032fa
SilentChaos512 <sworkman256@gmail.com>
2016-08-19 00:21:48 -0500

    Fix fluffy puff seeds crash (closes #58)

--- common/net/silentchaos512/gems/item/ItemFluffyPuffSeeds.java
+++ common/net/silentchaos512/gems/item/ItemFluffyPuffSeeds.java
@@ -4,10 +4,18 @@ import java.util.List;
 
 import com.google.common.collect.Lists;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemSeeds;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.silentchaos512.gems.SilentGems;
 import net.silentchaos512.gems.block.ModBlocks;
@@ -21,6 +29,30 @@ public class ItemFluffyPuffSeeds extends ItemSeeds implements IRegistryObject {
     super(ModBlocks.fluffyPuffPlant, Blocks.FARMLAND);
     setUnlocalizedName(Names.FLUFFY_PUFF_SEEDS);
     MinecraftForge.addGrassSeed(new ItemStack(this), 2);
+  }
+
+  @Override
+  public IBlockState getPlant(IBlockAccess world, BlockPos pos) {
+
+    return ModBlocks.fluffyPuffPlant.getDefaultState();
+  }
+
+  @Override
+  public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn,
+      BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+
+    // Identical to ItemSeeds.onItemUse, except using getPlant instead of this.crops, which
+    // does not work.
+    IBlockState state = worldIn.getBlockState(pos);
+    if (facing == EnumFacing.UP && playerIn.canPlayerEdit(pos.offset(facing), facing, stack)
+        && state.getBlock().canSustainPlant(state, worldIn, pos, EnumFacing.UP, this)
+        && worldIn.isAirBlock(pos.up())) {
+      worldIn.setBlockState(pos.up(), getPlant(worldIn, pos));
+      --stack.stackSize;
+      return EnumActionResult.SUCCESS;
+    } else {
+      return EnumActionResult.FAIL;
+    }
   }
 
   @Override
