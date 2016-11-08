de9c94ec8ea427802baa1be732609db94fc217e4
SilentChaos512 <sworkman256@gmail.com>
2016-11-08 01:02:35 -0600

    Maybe fix crash with Forge 2122 (#74)

--- common/net/silentchaos512/gems/entity/ModEntities.java
+++ common/net/silentchaos512/gems/entity/ModEntities.java
@@ -1,23 +1,39 @@
 package net.silentchaos512.gems.entity;
 
+import net.minecraft.entity.Entity;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.silentchaos512.gems.SilentGems;
-import net.silentchaos512.gems.entity.packet.EntityChaosNodePacket;
+import net.silentchaos512.gems.entity.packet.EntityPacketAttack;
+import net.silentchaos512.gems.entity.packet.EntityPacketChaos;
+import net.silentchaos512.gems.entity.packet.EntityPacketLevitation;
+import net.silentchaos512.gems.entity.packet.EntityPacketRegen;
+import net.silentchaos512.gems.entity.packet.EntityPacketRepair;
+import net.silentchaos512.gems.entity.packet.EntityPacketSaturation;
 
 public class ModEntities {
 
+  private static int ID;
+
   public static void init() {
 
-    int id = -1;
-    EntityRegistry.registerModEntity(EntityChaosNodePacket.class, "ChaosNodePacket", ++id,
-        SilentGems.instance, 64, 10, true);
-    EntityRegistry.registerModEntity(EntityChaosProjectile.class, "ChaosProjectile", ++id,
-        SilentGems.instance, 64, 20, true);
-    EntityRegistry.registerModEntity(EntityChaosProjectileHoming.class, "ChaosProjectileHoming",
-        ++id, SilentGems.instance, 64, 20, true);
-    EntityRegistry.registerModEntity(EntityChaosProjectileSweep.class, "ChaosProjectileSweep", ++id,
-        SilentGems.instance, 64, 20, true);
-    EntityRegistry.registerModEntity(EntityThrownTomahawk.class, "ThrownTomahawk", ++id,
-        SilentGems.instance, 64, 20, true);
+    ID = -1;
+
+    register(EntityChaosProjectile.class, "ChaosProjectile");
+    register(EntityChaosProjectileHoming.class, "ChaosProjectileHoming");
+    register(EntityChaosProjectileSweep.class, "ChaosProjectileSweep");
+
+    register(EntityThrownTomahawk.class, "ThrownTomahawk");
+
+    register(EntityPacketChaos.class, "NodePacketChaos");
+    register(EntityPacketRepair.class, "NodePacketRepair");
+    register(EntityPacketRegen.class, "NodePacketRegen");
+    register(EntityPacketSaturation.class, "NodePacketSaturation");
+    register(EntityPacketAttack.class, "NodePacketAttack");
+    register(EntityPacketLevitation.class, "NodePacketLeviation");
+  }
+
+  private static void register(Class<? extends Entity> clazz, String name) {
+
+    EntityRegistry.registerModEntity(clazz, name, ++ID, SilentGems.instance, 64, 20, true);
   }
 }
