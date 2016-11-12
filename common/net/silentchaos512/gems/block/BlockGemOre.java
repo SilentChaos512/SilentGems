4f87aa42f92c4425213713403c83cc7f2994d88e
SilentChaos512 <sworkman256@gmail.com>
2016-11-12 16:00:52 -0600

    Fix TheOneProbe display of dark gem ores (closes #78)

--- common/net/silentchaos512/gems/block/BlockGemOre.java
+++ common/net/silentchaos512/gems/block/BlockGemOre.java
@@ -4,10 +4,13 @@ import java.util.Random;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.OreDictionary;
 import net.silentchaos512.gems.item.ModItems;
@@ -62,6 +65,13 @@ public class BlockGemOre extends BlockGemSubtypes {
   public int damageDropped(IBlockState state) {
 
     return getMetaFromState(state) + (isDark ? 16 : 0);
+  }
+
+  @Override
+  public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos,
+      EntityPlayer player) {
+
+    return new ItemStack(this, 1, damageDropped(state) & 0xF);
   }
 
   @Override
