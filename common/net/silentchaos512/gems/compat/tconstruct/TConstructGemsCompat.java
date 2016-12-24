db6e63a3d747163e522d6e26f7267ce9e385118e
SilentChaos512 <sworkman256@gmail.com>
2016-12-24 03:08:49 -0600

    Add TiC support. WIP, no traits, may need balancing (cc #89)

--- common/net/silentchaos512/gems/compat/tconstruct/TConstructGemsCompat.java
+++ common/net/silentchaos512/gems/compat/tconstruct/TConstructGemsCompat.java
@@ -1,6 +1,15 @@
 package net.silentchaos512.gems.compat.tconstruct;
 
 import net.silentchaos512.gems.SilentGems;
+import net.silentchaos512.gems.api.lib.EnumMaterialTier;
+import net.silentchaos512.gems.lib.EnumGem;
+import slimeknights.tconstruct.TinkerIntegration;
+import slimeknights.tconstruct.library.TinkerRegistry;
+import slimeknights.tconstruct.library.materials.ArrowShaftMaterialStats;
+import slimeknights.tconstruct.library.materials.BowMaterialStats;
+import slimeknights.tconstruct.library.materials.ExtraMaterialStats;
+import slimeknights.tconstruct.library.materials.HandleMaterialStats;
+import slimeknights.tconstruct.library.materials.HeadMaterialStats;
 
 public class TConstructGemsCompat {
 
@@ -8,6 +17,32 @@ public class TConstructGemsCompat {
 
     SilentGems.logHelper.info("Loading TConstruct compatibility module...");
 
-    
+    for (EnumGem gem : EnumGem.values())
+      register(gem, EnumMaterialTier.REGULAR);
+    for (EnumGem gem : EnumGem.values())
+      register(gem, EnumMaterialTier.SUPER);
+  }
+
+  private static void register(EnumGem gem, EnumMaterialTier tier) {
+
+    TConstructMaterialGem mat = new TConstructMaterialGem(gem, tier);
+    TinkerIntegration.integrate(mat,
+        tier == EnumMaterialTier.SUPER ? gem.getItemSuperOreName() : gem.getItemOreName());
+
+    int durability = gem.getDurability(tier);
+    float miningSpeed = gem.getMiningSpeed(tier);
+    float meleeDamage = gem.getMeleeDamage(tier);
+    float meleeSpeed = gem.getMeleeSpeed(tier);
+    int harvestLevel = gem.getHarvestLevel(tier);
+    float drawDelay = Math.max(38.4f - 1.4f * meleeSpeed * miningSpeed, 10);
+
+    TinkerRegistry.addMaterialStats(mat, new HeadMaterialStats(durability, miningSpeed, meleeDamage, harvestLevel));
+    TinkerRegistry.addMaterialStats(mat, new HandleMaterialStats(0.875f, durability / 8));
+    TinkerRegistry.addMaterialStats(mat, new ExtraMaterialStats(durability / 8));
+    TinkerRegistry.addMaterialStats(mat, new BowMaterialStats(20f / drawDelay, 0.4f * meleeDamage - 4, meleeDamage));
+    //TinkerRegistry.addMaterialStats(mat, new BowStringMaterialStats(1f));
+    TinkerRegistry.addMaterialStats(mat, new ArrowShaftMaterialStats(1.0f, 0));
+    //TinkerRegistry.addMaterialStats(mat, new FletchingMaterialStats(1f, 1f));
+    //TinkerRegistry.addMaterialStats(mat, new ProjectileMaterialStats());
   }
 }
--- common/net/silentchaos512/gems/compat/tconstruct/TConstructMaterialGem.java
+++ common/net/silentchaos512/gems/compat/tconstruct/TConstructMaterialGem.java
@@ -1,14 +1,34 @@
 package net.silentchaos512.gems.compat.tconstruct;
 
+import net.minecraft.item.ItemStack;
+import net.silentchaos512.gems.SilentGems;
+import net.silentchaos512.gems.api.lib.EnumMaterialTier;
 import net.silentchaos512.gems.lib.EnumGem;
 import slimeknights.tconstruct.library.materials.Material;
 
 public class TConstructMaterialGem extends Material {
 
-  public TConstructMaterialGem(EnumGem gem) {
+  EnumGem gem;
+  EnumMaterialTier tier;
 
-    super("silentgems:" + gem.name().toLowerCase(), gem.getColor());
-    // TODO Auto-generated constructor stub
+  public TConstructMaterialGem(EnumGem gem, EnumMaterialTier tier) {
+
+    super("silentgems:" + gem.name().toLowerCase() + (tier == EnumMaterialTier.SUPER ? "_super" : ""), gem.getColor());
+    this.gem = gem;
+    this.tier = tier;
+    setCraftable(true);
   }
 
+  @Override
+  public ItemStack getRepresentativeItem() {
+
+    return gem.getItem();
+  }
+
+  @Override
+  public String getLocalizedName() {
+
+    int meta = gem.ordinal() + (tier == EnumMaterialTier.SUPER ? 32 : 0); 
+    return SilentGems.localizationHelper.getLocalizedString("item", "Gem" + meta + ".name");
+  }
 }
