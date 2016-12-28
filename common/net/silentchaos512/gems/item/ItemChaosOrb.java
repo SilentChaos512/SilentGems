25f92e130cba51b2d25010d83e99f0be7b07ad53
SilentChaos512 <sworkman256@gmail.com>
2016-12-28 12:59:18 -0600

    Chaos Orbs recharge items, refined tier more durable

--- common/net/silentchaos512/gems/item/ItemChaosOrb.java
+++ common/net/silentchaos512/gems/item/ItemChaosOrb.java
@@ -23,6 +23,7 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.silentchaos512.gems.SilentGems;
+import net.silentchaos512.gems.api.energy.IChaosStorage;
 import net.silentchaos512.gems.handler.PlayerDataHandler;
 import net.silentchaos512.gems.handler.PlayerDataHandler.PlayerData;
 import net.silentchaos512.gems.lib.Names;
@@ -37,7 +38,7 @@ public class ItemChaosOrb extends ItemChaosStorage {
     //@formatter:off
     POTATO(     5000, 0.01f, 1),
     FRAGILE(  100000, 0.20f, 3),
-    REFINED( 1000000, 0.05f, 3),
+    REFINED( 1000000, 0.05f, 5),
     SUPREME(10000000, 0.00f, 1);
     //@formatter:on
 
@@ -54,6 +55,8 @@ public class ItemChaosOrb extends ItemChaosStorage {
   }
 
   public static final String NBT_CHARGE = "ChaosCharge";
+
+  public static final int MAX_ITEM_SEND = 2000;
 
   public ItemChaosOrb() {
 
@@ -179,18 +182,35 @@ public class ItemChaosOrb extends ItemChaosStorage {
       EntityPlayer player = (EntityPlayer) entityIn;
       PlayerData data = PlayerDataHandler.get(player);
 
+      // Send Chaos to player
       int amount = extractCharge(stack, data.getChaosChargeSpeed(), true);
       amount = data.sendChaos(amount);
       extractCharge(stack, amount, false);
 
-      // if (amount > 0) {
-      // SilentGems.instance.logHelper.debug(amount, getCharge(stack), getMaxCharge(stack),
-      // getBreakChance(stack));
-      // }
-
-      float breakTries = (int) (amount / PlayerData.CHAOS_MAX_TRANSFER);
+      // Damage from player send?
+      int breakTries = amount / PlayerData.CHAOS_MAX_TRANSFER;
       for (int i = 0; i < breakTries; ++i) {
-        if (amount > 0 && SilentGems.instance.random.nextFloat() < getBreakChance(stack)) {
+        if (amount > 0 && SilentGems.random.nextFloat() < getBreakChance(stack)) {
+          damageOrb(stack, player);
+          break;
+        }
+      }
+
+      // Try recharge player's items.
+      int totalSentToItems = 0;
+      for (ItemStack itemstack : PlayerHelper.getNonNullStacks(player)) {
+        if (itemstack.getItem() instanceof IChaosStorage && itemstack.getItem() != this) {
+          int toSend = Math.min(getCharge(stack), MAX_ITEM_SEND);
+          totalSentToItems += ((IChaosStorage) itemstack.getItem()).receiveCharge(itemstack, toSend,
+              false);
+        }
+      }
+      extractCharge(stack, totalSentToItems, false);
+
+      // Damage from item send? (lower damage chance)
+      breakTries = totalSentToItems / MAX_ITEM_SEND;
+      for (int i = 0; i < breakTries; ++i) {
+        if (SilentGems.random.nextFloat() < getBreakChance(stack) / 3) {
           damageOrb(stack, player);
           break;
         }
@@ -236,8 +256,8 @@ public class ItemChaosOrb extends ItemChaosStorage {
     }
 
     // Chat notification.
-    String line = SilentGems.instance.localizationHelper.getItemSubText(itemName, "crack");
-    line = String.format(line, stack.getDisplayName());
+    String line = SilentGems.localizationHelper.getItemSubText(itemName, "crack",
+        stack.getDisplayName());
     PlayerHelper.addChatMessage(player, line);
 
     // Glass breaking sound.
@@ -248,9 +268,9 @@ public class ItemChaosOrb extends ItemChaosStorage {
   public void breakOrb(ItemStack stack, EntityPlayer player) {
 
     // Chat notification.
-    String line = SilentGems.instance.localizationHelper.getItemSubText(itemName, "break");
-    int pieceCount = SilentGems.instance.random.nextInt(99000) + 1000;
-    line = String.format(line, stack.getDisplayName(), pieceCount);
+    int pieceCount = SilentGems.random.nextInt(99000) + 1000;
+    String line = SilentGems.localizationHelper.getItemSubText(itemName, "break",
+        stack.getDisplayName(), pieceCount);
     PlayerHelper.addChatMessage(player, line);
 
     // Glass breaking sound.
