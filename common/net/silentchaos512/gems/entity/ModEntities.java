edeba0526050b2f729a43aaea4dbee875cdbcf0a
SilentChaos512 <sworkman256@gmail.com>
2017-11-01 22:56:36 -0500

    Make ender slime spawn rate more sane

--- common/net/silentchaos512/gems/entity/ModEntities.java
+++ common/net/silentchaos512/gems/entity/ModEntities.java
@@ -22,6 +22,6 @@ public class ModEntities {
     reg.registerEntity(EntityThrownTomahawk.class, "ThrownTomahawk");
 
     reg.registerEntity(EntityEnderSlime.class, "EnderSlime", 64, 4, false, 0x003333, 0xAA00AA);
-    EntityRegistry.addSpawn(EntityEnderSlime.class, 50, 1, 4, EnumCreatureType.MONSTER, Biomes.SKY);
+    EntityRegistry.addSpawn(EntityEnderSlime.class, 10, 1, 4, EnumCreatureType.MONSTER, Biomes.SKY);
   }
 }
