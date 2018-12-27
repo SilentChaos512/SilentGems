31e742ee7ecf02404fb8dc730b689b7d7ef1d701
SilentChaos512 <sworkman256@gmail.com>
2018-12-27 12:10:04 -0600

    Add missing armor textures, fix file names

--- src/main/java/net/silentchaos512/gems/SilentGems.java
+++ src/main/java/net/silentchaos512/gems/SilentGems.java
@@ -50,7 +50,7 @@ public class SilentGems implements IModBase {
     public static final String MODID = "silentgems";
     public static final String MODID_NBT = "SilentGems"; // The original ID, used in NBT.
     public static final String MOD_NAME = "Silent's Gems";
-    public static final String VERSION = "2.8.11";
+    public static final String VERSION = "2.8.12";
     public static final String VERSION_SILENTLIB = "3.0.9";
     public static final int BUILD_NUM = 0;
     public static final String DEPENDENCIES = "required-after:silentlib@[" + VERSION_SILENTLIB + ",);"
